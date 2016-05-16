372770593c6ed615f7093fb48a2d0a47182a78bd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-05-16 09:37:50 -0400

    Initial Commit

---
+++ COMP125-Lesson2/Scripts/app.js
@@ -0,0 +1,17 @@
+﻿/* main JavaScript file */
+
+// IIFE - Immediately Invoked Function Expression
+(function () {
+    "use strict";
+
+
+    // code codes in here 
+    console.log("App Started...");
+
+
+
+
+
+
+
+})();
